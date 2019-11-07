ea2516bcb37a6906bee97c5bf9c1e6e93831ffbd
Sanket Khandekar <sanket.khandekar@global.internal>
2019-11-07 11:46:40 +0530

    Spring Boot Project Initial checked in

---
+++ src/main/java/code/onlycode/springbootsecurity/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package code.onlycode.springbootsecurity;
+
+public class SecurityConfiguration{
+	
+}
+/*@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Set your configuration on the auth object
+        auth.inMemoryAuthentication()
+                .withUser("blah")
+                .password("blah")
+                .roles("USER")
+                .and()
+                .withUser("foo")
+                .password("foo")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+    }
+}*/
